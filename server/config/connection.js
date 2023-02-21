532261b56232642b6962fc4a4e9da9ecd281d3c8
Joey Lau <joeylau.2020@scis.smu.edu.sg>
2023-02-21 12:14:48 +0800

    Fix secrets

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql2');
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "root",
+  password: "",
   port: 8889,
   database: "url_shortener"
 });
